0bccb8c993f69f4934c1c709477b7736e901811b
Vaibhav Puri <vaibhav.puri26@gmail.com>
2024-07-13 04:39:31 +0530

    Token Resolve

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -2,10 +2,14 @@ import jwt from 'jsonwebtoken';
 import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.access_token;
-   
-  if (!token) return next(errorHandler(401, 'Unauthorized'));
-
+  const authHeader = req.headers['authorization'];
+  
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return next(errorHandler(401, 'Unauthorized'));
+  }
+  
+  const token = authHeader.split(' ')[1];
+  
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return next(errorHandler(403, 'Forbidden'));
 
