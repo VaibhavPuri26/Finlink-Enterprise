67a932f4a510f1c4e7d066c97464f3de7a96bb93
Vaibhav Puri <vaibhav.puri26@gmail.com>
2024-07-13 05:23:35 +0530

    Delete File

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
